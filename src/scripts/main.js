8501d12730ba3be710ef14573f8368167502b898
igorprogrammer93 <igormtba@gmail.com>
2024-12-09 20:42:49 -0300

    timeevento

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-const dataDoEvento = new Date("Dec 12, 2022 19:00:00");
+const dataDoEvento = new Date("jan 05, 2025 15:00:00");
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(function() {
